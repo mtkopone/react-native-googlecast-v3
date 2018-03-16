ea27fa440f1ed798e69bb51f3ef25207165543af
Mikko Koponen <mikko.koponen@reaktor.fi>
2018-03-16 18:14:36 +0200

    Fake it for iOS for now

--- googlecast-v3.js
+++ googlecast-v3.js
@@ -1,10 +1,10 @@
 import React, { PureComponent } from 'react'
-import ReactNative, { DeviceEventEmitter, NativeModules, requireNativeComponent, UIManager, ViewPropTypes } from 'react-native'
+import ReactNative, { Platform, DeviceEventEmitter, NativeModules, requireNativeComponent, UIManager, View, ViewPropTypes } from 'react-native'
 import PropTypes from 'prop-types'
 
 const NativeGoogleCastV3 = NativeModules.GoogleCastV3
 
-export const GoogleCastV3 = {
+const GoogleCastV3Handler = {
   ...NativeGoogleCastV3,
 
   send: (a, b) => {
@@ -19,6 +19,21 @@ export const GoogleCastV3 = {
   },
   addCastMessageListener: (fn) => DeviceEventEmitter.addListener('googleCastMessage', fn),
 }
+
+const stub = {
+  NO_DEVICES_AVAILABLE: 1,
+  NOT_CONNECTED: 2,
+  CONNECTING: 3,
+  CONNECTED: 4,
+  send: () => {},
+  addCastStateListener: (fn) => {
+    fn(stub.NO_DEVICES_AVAILABLE)
+  },
+  addCastMessageListener: () => {},
+  getCurrentDevice: () => Promise.resolve(null)
+}
+
+export const GoogleCastV3 = (Platform.OS === 'android') ? GoogleCastV3Handler : stub
 
 class CastButton extends PureComponent {
   static propTypes = {
@@ -39,9 +54,8 @@ class CastButton extends PureComponent {
       <NativeCastButton {...this.props} />
     );
   }
-
 }
 
 const NativeCastButton = requireNativeComponent('CastButton', CastButton)
 
-export default CastButton
+export default (Platform.OS === 'android') ? CastButton : View
